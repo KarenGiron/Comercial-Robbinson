03e31bc0a6efbd78d9c003f0446125d3c840d62d
FrancisAzpur <u21229536@utp.edu.pe>
2026-01-27 17:44:03 -0500

    ComerceRobbinson

---
+++ src/main/java/com/Robbinson/ComRobinson/ComRobinsonApplication.java
@@ -0,0 +1,13 @@
+package com.Robbinson.ComRobinson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComRobinsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComRobinsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Robbinson/ComRobinson/ComRobinsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Robbinson.ComRobinson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComRobinsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
